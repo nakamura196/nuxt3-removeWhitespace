1f9655ccb9d53ca591e0f8dac5bdde1c22acf1c8
Satoru <you@example.com>
2022-10-25 18:47:36 +0900

    feat: add pages

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <h1>v-removeWhitespace なし</h1>
-  <div>
-    お問い合わせは
-    <a href="#">こちらから</a>
-    お願いします
-  </div>
-
-  <br/>
-
-  <h1>v-removeWhitespace あり</h1>
-  <div v-removeWhitespace>
-    お問い合わせは
-    <a href="#">こちらから</a>
-    お願いします
-  </div>
+    <NuxtPage />
 </div>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nuxt-link to="/">トップへ</nuxt-link>
+
+    <h1>v-removeWhitespace なし</h1>
+    <div>
+      お問い合わせは
+      <a href="#">こちらから</a>
+      お願いします
+    </div>
+
+    <br />
+
+    <h1>v-removeWhitespace あり</h1>
+    <div v-removeWhitespace>
+      お問い合わせは
+      <a href="#">こちらから</a>
+      お願いします
+    </div>
+  </div>
+</template>
