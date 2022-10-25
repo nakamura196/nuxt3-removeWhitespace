477805c6e91db498bd64cf1e50403bfbe1c9cfbc
Satoru <you@example.com>
2022-10-25 16:54:31 +0900

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>v-removeWhitespace なし</h1>
+  <div>
+    お問い合わせは
+    <a href="#">こちらから</a>
+    お願いします
+  </div>
+
+  <br/>
+
+  <h1>v-removeWhitespace あり</h1>
+  <div v-removeWhitespace>
+    お問い合わせは
+    <a href="#">こちらから</a>
+    お願いします
+  </div>
+</div>
+</template>
---
+++ plugins/removeWhitespace.ts
@@ -0,0 +1,19 @@
+function isText(node: ChildNode) {
+    return node.nodeType === Node.TEXT_NODE
+}
+
+function trimText(node: NodeListOf<ChildNode>) {
+    Array.from(node).forEach((node) => {
+        if (isText(node) && node.textContent) {
+            node.textContent = node.textContent.trim()
+            return
+        }
+        trimText(node.childNodes)
+    })
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("removeWhitespace", (el: HTMLElement) => {
+    trimText(el.childNodes)
+  });
+});
